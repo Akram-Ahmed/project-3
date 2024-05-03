70640b953c6a001705df285d117c906a59c2d8fd
Akram-Ahmed <akram.raeeini@hotmail.com>
2024-05-03 16:53:13 -0700

    project 3

--- dgemv-basic.cpp
+++ dgemv-basic.cpp
@@ -1,4 +1,5 @@
 const char* dgemv_desc = "Basic implementation of matrix-vector multiply.";
+#include <iostream>
 
 /*
  * This routine performs a dgemv operation
@@ -6,6 +7,15 @@ const char* dgemv_desc = "Basic implementation of matrix-vector multiply.";
  * where A is n-by-n matrix stored in row-major format, and X and Y are n by 1 vectors.
  * On exit, A and X maintain their input values.
  */
-void my_dgemv(int n, double* A, double* x, double* y) {
-   // insert your code here: implementation of basic matrix multiply
+void my_dgemv(int n, double* A, double* x, double* y) { 
+   for (int i = 0; i < n; i++){ // row
+      int offSet = i * n; 
+      double sum = 0.0;
+      for (int j = 0; j < n; j++){ // column
+         sum += A[offSet+ j] * x[j];
+      }
+      y[i] += sum;
+   }
 }
+
+
--- dgemv-openmp.cpp
+++ dgemv-openmp.cpp
@@ -13,18 +13,15 @@ const char* dgemv_desc = "OpenMP dgemv.";
  */
 
 void my_dgemv(int n, double* A, double* x, double* y) {
-
-   #pragma omp parallel
-   {
-      int nthreads = omp_get_num_threads();
-      int thread_id = omp_get_thread_num();
-      printf("my_dgemv(): Hello world: thread %d of %d checking in. \n", thread_id, nthreads);
-      printf("my_dgemv(): For actual timing runs, please comment out these printf() and omp_get_*() statements. \n");
-   }
-
-   // insert your dgemv code here. you may need to create additional parallel regions,
-   // and you will want to comment out the above parallel code block that prints out
-   // nthreads and thread_id so as to not taint your timings
-
+      #pragma omp parallel for 
+      for (int i = 0; i < n; i++){ // row
+         int offSet = i * n; 
+         double sum = 0.0;
+         #pragma omp parallel for reduction(+:sum)
+         for (int j = 0; j < n; j++){ // column
+            sum += A[offSet + j] * x[j];
+         }
+         y[i] += sum;
+      }
 }
 
